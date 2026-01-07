63545b97f5c49cbd1bd36fa1a7d695eb78343fa7
Kim Mi <km0398@nate.com>
2026-01-07 23:21:34 +0900

    Update __init__.py

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -1,12 +1,3 @@
-<table class="tfo-notebook-buttons" align="left">
-  <td>
-    <a target="_blank" href="https://nbviewer.jupyter.org/github/rickiepark/hg-da/blob/main/01-3.ipynb"><img src="https://jupyter.org/assets/share.png" width="61" />주피터 노트북 뷰어로 보기</a>
-  </td>
-  <td>
-    <a target="_blank" href="https://colab.research.google.com/github/rickiepark/hg-da/blob/main/01-3.ipynb"><img src="https://www.tensorflow.org/images/colab_logo_32px.png" />구글 코랩(Colab)에서 실행하기</a>
-  </td>
-</table>
-#--------------------------------------------------------------------------------------------------------
 from __future__ import annotations
 
 from typing import Any
